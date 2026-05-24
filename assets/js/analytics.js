d7e6ef103e3aa7b3278c539bf984dd235f72ff44
tmmygu40-dot <abc123@gmail.com>
2026-05-25 00:11:37 +0900

    feat: add GA4 tracking to initial pages

---
+++ assets/js/analytics.js
@@ -0,0 +1,12 @@
+(function(){
+  var GA_ID = 'G-N185HTL838';
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=' + GA_ID;
+  document.head.appendChild(s);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', GA_ID);
+})();
